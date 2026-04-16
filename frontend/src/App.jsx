346d8de4ab76d3f505f16c0befd54b7b597b4255
yasumoto-dev <yasumoto.ryo01@gmail.com>
2026-04-16 10:29:37 +0900

    write a App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import RoomNew from './pages/RoomNew'
+import RoomJoin from './pages/RoomJoin'
+import PostNew from './pages/PostNew'
+import PostList from './pages/PostList'
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/rooms/new" element={<RoomNew />} />
+                <Route path="/rooms/join" element={<RoomJoin />} />
+                <Route path="/rooms/:roomId/posts/new" element={<PostNew />} />
+                <Route path="/rooms/:roomId/posts" element={<PostList />} />
+
+            </Routes>
+        </BrowserRouter>
+    );
+}
