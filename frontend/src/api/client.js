7dcb976f5d92c5787c1c3e8b2df954b40a947aa5
yasumoto-dev <yasumoto.ryo01@gmail.com>
2026-04-16 10:11:42 +0900

    write a client.js

---
+++ frontend/src/api/client.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+// Axiosインスタンスの作成
+const client = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+})
+
+// APIのエンドポイントの定義
+export const createRoom = (data) => {
+    return client.post('/api/rooms', data)
+};
+export const joinRoom = (roomId, data) => {
+    return client.post(`/api/rooms/${roomId}/join`, data)
+};
+export const createPost = (roomId, data) => {
+    return client.post(`api/rooms/${roomId}/posts`, data)
+};
+export const getPosts = (roomId) => {
+    return client.get(`/api/rooms/${roomId}/posts`)
+};
